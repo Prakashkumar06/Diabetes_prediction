48b96b9ddaf50c8c578a586cbe7493a42b6bf3bc
Prakashkumar06 <prakashkumarsingh213@gmail.com>
2024-04-26 05:20:35 -0700

    first commit

---
+++ Diabetes-Deployment-With-BeanStalk-main/application.py
@@ -0,0 +1,55 @@
+from flask import Flask, request, app,render_template
+from flask import Response
+import os
+import pickle
+import numpy as np 
+import pandas as pd
+
+model_dir = os.path.join(os.path.dirname(__file__), 'Model')
+scaler = pickle.load(open(os.path.join(model_dir, "standardScalar.pkl"), "rb"))
+
+
+application = Flask(__name__)
+app=application
+
+scaler=pickle.load(open("C:\\Users\\HP\\Desktop\\Diabetes-Deployment-With-BeanStalk-main (1) (2)\\Diabetes-Deployment-With-BeanStalk-main\\/Model/standardScalar.pkl", "rb"))
+model = pickle.load(open("C:\\Users\\HP\\Desktop\\Diabetes-Deployment-With-BeanStalk-main (1) (2)\\Diabetes-Deployment-With-BeanStalk-main\\/Model/modelForPrediction.pkl", "rb"))
+
+## Route for homepage
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+## Route for Single data point prediction
+@app.route('/predictdata',methods=['GET','POST'])
+def predict_datapoint():
+    result=""
+
+    if request.method=='POST':
+
+        Pregnancies=int(request.form.get("Pregnancies"))
+        Glucose = float(request.form.get('Glucose'))
+        BloodPressure = float(request.form.get('BloodPressure'))
+        SkinThickness = float(request.form.get('SkinThickness'))
+        Insulin = float(request.form.get('Insulin'))
+        BMI = float(request.form.get('BMI'))
+        DiabetesPedigreeFunction = float(request.form.get('DiabetesPedigreeFunction'))
+        Age = float(request.form.get('Age'))
+
+        new_data=scaler.transform([[Pregnancies,Glucose,BloodPressure,SkinThickness,Insulin,BMI,DiabetesPedigreeFunction,Age]])
+        predict=model.predict(new_data)
+       
+        if predict[0] ==1 :
+            result = 'Diabetic'
+        else:
+            result ='Non-Diabetic'
+            
+        return render_template('single_prediction.html',result=result)
+
+    else:
+        return render_template('home.html')
+
+
+if __name__=="__main__":
+    app.run(host="0.0.0.0")
